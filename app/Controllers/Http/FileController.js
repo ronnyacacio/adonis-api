b98ed47b5b5c185e5b7cd98be2c5d02825eb4470
Ronny Acacio <ronnyacacio27@gmail.com>
2020-07-18 15:36:56 -0300

    feat: file upload

---
+++ app/Controllers/Http/FileController.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const Helpers = use('Helpers');
+const File = use('App/Models/File');
+
+class FileController {
+  async store({ request, response }) {
+    try {
+      if (!request.file('file')) return;
+
+      const upload = request.file('file', { size: '2mb' });
+
+      const filename = `${Date.now()}.${upload.subtype}`;
+
+      await upload.move(Helpers.tmpPath('uploads'), {
+        name: filename,
+      });
+
+      if (!upload.moved()) throw upload.error();
+
+      const file = await File.create({
+        file: filename,
+        name: upload.clientName,
+        type: upload.type,
+        subtype: upload.subtype,
+      });
+
+      return file;
+    } catch (err) {
+      return response.status(err.status).send({ error: 'erro' });
+    }
+  }
+}
+
+module.exports = FileController;
